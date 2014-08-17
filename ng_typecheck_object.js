2a20c967ebaae7e7285442ea4997cc232aad1243
Emmanuel DEMEY <emmanuel.demey@atos.net>
2014-08-17 22:32:40 +0200

    Add AngularJS rules

---
+++ ng_typecheck_object.js
@@ -0,0 +1,31 @@
+module.exports = function(context) {
+
+    "use strict";
+
+    function recordError(node, origin){
+    	if(node.type === "Literal" && node.value === "[object Object]") {
+    		context.report(origin, "You should use the angular.isObject method", {});
+    	}
+    }
+
+    return {
+        
+        "BinaryExpression": function(node) {
+        	
+            if(node.operator === "===" || node.operator === "!=="){
+                if(node.left.type === "UnaryExpression" && node.left.operator === "typeof"){
+                    recordError(node.right, node);
+                }
+                else if(node.right.type === "UnaryExpression" && node.right.operator === "typeof"){
+                    recordError(node.left, node);
+                }
+                else {
+                	recordError(node.right, node);
+                	recordError(node.left, node);
+                }
+            }
+
+        }
+    };
+
+};
